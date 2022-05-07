66e59f9b5bc8e45225c2c3ab7cd508c8302d7721
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-05-07 21:01:41 +0500

    1st class flutter

---
+++ Dart/Lectures/Lec 01 variables And Datatypes.dart
@@ -0,0 +1,31 @@
+void main(){
+  
+    int abc = 123;
+  print(abc);
+  
+    double abcd = 123.5;
+  print(abcd);
+  
+    num abcnum = 123;
+  print(abcnum);
+  
+    num abcdnum = 123.4;
+  print(abcdnum);
+  
+  
+ 
+String name ="daniyal";
+    print(name);
+String address = "Street No 123 abc karachi ";
+  print(address);
+String date = "28-March-2021";
+  print(date);
+String email = "maryam@gmail.com";
+  print(email);
+  
+  
+  
+  
+  
+  
+}
